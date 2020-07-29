0dbcb7ed3dde1aa68278ad07edf172b788e02e08
KhanhLe <khanhle@brodev.com>
2020-07-29 23:21:59 +0700

    Updates

---
+++ precache-manifest.f40a1570e8a3942740db1d3773f45863.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc4165f7ebfe276757494a5d48d851f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7a0c59126118c2b00b2b",
+    "url": "/monsters-rolodex/static/css/main.4fa586dd.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a0c59126118c2b00b2b",
+    "url": "/monsters-rolodex/static/js/main.b157e626.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
